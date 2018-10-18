f48e233d60e91645a6feba0af296e28eb3977e8c
joao barbirato <joaobarbirato@gmail.com>
2018-10-17 21:56:45 -0300

    Adicionadas funcionalidades basicas nas classes do programa

--- src/main/python/programa.py
+++ src/main/python/programa.py
@@ -3,8 +3,28 @@ import PIL
 
 class Posicao():
     def __init__(self,x=None,y=None):
-        self.x = x
-        self.y = y
+        self.__x = x
+        self.__y = y
+    
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
+
+    def set_x(self, x=0):
+        if x >= 0:
+            self.__x = x
+            return True
+
+        return False
+
+    def set_y(self, y=0):
+        if y >= 0:
+            self.__y = y
+            return True
+
+        return False
 
 
 class Elemento():
@@ -25,7 +45,31 @@ class Cor(elemento):
     """
         Classe de cores
     """
-    pass
+    def __init__(self, cor=(0,0,0)):
+        if isinstance(cor, tuple) and len(tuple) == 3:
+            (self.__r, self.__g, self.__b) = tuple
+            self.__cor = tuple
+            self.__tipo_cor = "rgb"
+        else:
+            self.__cor = tuple
+            self.__tipo_cor = "hex"
+
+    def get_cor(self):
+        return self.__cor
+        
+
+    def set_cor(self, cor=(0,0,0)):
+        if isinstance(cor, tuple) and len(tuple) == 3:
+            (self.__r, self.__g, self.__b) = tuple
+            self.__cor = tuple
+            self.__tipo_cor = "rgb"
+            return True
+        else:
+            self.__cor = tuple
+            self.__tipo_cor = "hex"
+            return True
+
+        return False
 
 
 class Programa():
@@ -33,10 +77,8 @@ class Programa():
         Classe principal de comandos do programa
     """
     def __init__(self):
+
         pass
 
-    def declare_wallpaper(self):
-        pass
-
-    def declare_cor(self):
+    def declare_elemento(self, elemento):
         pass
