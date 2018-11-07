f250b6ee678f6583c768f4565bd5fb365c0a63e7
rebecalrocha <rochalrebeca@gmail.com>
2018-11-06 22:34:31 -0200

    Adicionado opção de referenciar formas.

--- src/main/wallpaper/Semantico.py
+++ src/main/wallpaper/Semantico.py
@@ -24,7 +24,7 @@ class Semantico(wallpaperVisitor):
         else:
             self.imagens.addTabela(TabelaSimbolo(ctx.IDENT().getText()))
 
-    def visitCorpo(self, ctx:wallpaperParser.CorpoContext):
+    def visitCorpo(self, ctx: wallpaperParser.CorpoContext):
         if not self.imagens.exist(ctx.IDENT()):
             print('Erro: O identificador ' + str(ctx.IDENT()) + ' não foi declarado.')
         else:
@@ -42,48 +42,80 @@ class Semantico(wallpaperVisitor):
         elif ctx.tamanho():
             tam = ctx.tamanho()
             if not self.tabela_imagem.getSimbolo('tamanho'):
-                self.tabela_imagem.addSimbolo(Simbolo('tamanho', (int(tam.NUM_INT(0).getText()), int(tam.NUM_INT(1).getText()))))
+                self.tabela_imagem.addSimbolo(
+                    Simbolo('tamanho', (int(tam.NUM_INT(0).getText()), int(tam.NUM_INT(1).getText()))))
             else:
                 print('Erro: O tamanho já foi adicionado à imagem.')
                 return
 
         elif ctx.nome_arquivo():
             if not self.tabela_imagem.getSimbolo('nome'):
-                self.tabela_imagem.addSimbolo(Simbolo('nome', ctx.nome_arquivo().IDENT().getText() + '.' + ctx.nome_arquivo().tipo_arquivo().getText()))
+                self.tabela_imagem.addSimbolo(Simbolo('nome',
+                                                      ctx.nome_arquivo().IDENT().getText() + '.' + ctx.nome_arquivo().tipo_arquivo().getText()))
             else:
                 print('Erro: Imagem já possui um nome de arquivo.')
                 return
+
 
         elif ctx.conteudo():
             for conteudo in self.conteudoImagem:
                 if conteudo == self.tabela_imagem.nome_tabela:
                     print('Imagem já possui um conteúdo.')
                     return
+
             self.conteudoImagem.append(self.tabela_imagem.nome_tabela)
             self.visitConteudo(ctx.conteudo())
 
     def visitConteudo(self, ctx: wallpaperParser.ConteudoContext):
-        i = 0
-        for ctxForma in ctx.forma():
-            self.visitAtributos(ctx.atributos(i))
-            self.tabela_forma.addSimbolo(Simbolo('formato', ctxForma.getText()))
-            i += 1
+        for referencia in ctx.referencia():
+            self.visitReferencia(referencia)
 
-    # TODO: Verificar para cada forma que ela somente pode ter
-    # TODO: 1 cor, 1 posicao, 1 chave etc...
+        for valor in ctx.valores():
+            self.visitValores(valor)
+
+    def visitReferencia(self, ctx: wallpaperParser.ReferenciaContext):
+        # verificar primeiro IDENT tabela de imagens
+        # verificar segundo IDENT tabela de formas
+        # verificar terceiro IDENT que é o chave da nova forma
+
+        copy_forma = self.formas.getTabela(ctx.IDENT(1))
+        self.tabela_forma = TabelaSimbolo(ctx.IDENT(2).getText())
+        self.tabela_forma.addSimbolo(Simbolo('formato', copy_forma.getSimbolo('formato').valor))
+
+        if ctx.cor():
+            self.tabela_forma.addSimbolo(Simbolo('cor', ctx.cor().HEX()))
+        else:
+            self.tabela_forma.addSimbolo(Simbolo('cor', copy_forma.getSimbolo('cor').valor))
+
+        if ctx.posicao():
+            posicao = ctx.posicao()
+            self.tabela_forma.addSimbolo(
+                Simbolo('posicao', (int(posicao.NUM_INT(0).getText()), int(posicao.NUM_INT(1).getText()),
+                                    int(posicao.NUM_INT(2).getText()), int(posicao.NUM_INT(3).getText()))))
+        else:
+            self.tabela_forma.addSimbolo(Simbolo('posicao', copy_forma.getSimbolo('posicao').valor))
+
+        self.formas.addTabela(self.tabela_forma)
+        self.tabela_forma = None
+
+    def visitValores(self, ctx: wallpaperParser.ValoresContext):
+        self.visitAtributos(ctx.atributos())
+        self.tabela_forma.addSimbolo(Simbolo('formato', ctx.forma().getText()))
+
     def visitAtributos(self, ctx: wallpaperParser.AtributosContext):
 
         if not ctx.chave().getText():
             print('Erro: O atributo chave é obrigatório para formas')
-            return #exit()
+            return  # exit()
 
         # Verifica se já foi declarado o identificador da forma (chave)
         if self.formas.exist(ctx.chave().IDENT()) or self.imagens.exist(ctx.chave().IDENT()):
             print('Erro: O identificador ' + ctx.chave().IDENT().getText() + ' já foi declarado.')
             return
         else:
-            # adiciona uma entrada do tipo forma na tabela de simbolos da imagem
-            self.tabela_imagem.addSimbolo(Simbolo('forma', ctx.chave().IDENT().getText()))
+            # adiciona uma entrada do tipo chave na tabela de simbolos da imagem
+            self.tabela_imagem.addSimbolo(Simbolo('chave', ctx.chave().IDENT().getText()))
+
             # atribui a tabela de forma para a tabela auxiliar
             self.tabela_forma = TabelaSimbolo(ctx.chave().IDENT().getText())
             self.formas.addTabela(self.tabela_forma)
@@ -99,6 +131,6 @@ class Semantico(wallpaperVisitor):
             return
 
         posicao = ctx.posicao()
-        self.tabela_forma.addSimbolo(Simbolo('posicao', (int(posicao.NUM_INT(0).getText()), int(posicao.NUM_INT(1).getText()),
-                                                         int(posicao.NUM_INT(2).getText()), int(posicao.NUM_INT(3).getText()) )))
-
+        self.tabela_forma.addSimbolo(
+            Simbolo('posicao', (int(posicao.NUM_INT(0).getText()), int(posicao.NUM_INT(1).getText()),
+                                int(posicao.NUM_INT(2).getText()), int(posicao.NUM_INT(3).getText()))))
